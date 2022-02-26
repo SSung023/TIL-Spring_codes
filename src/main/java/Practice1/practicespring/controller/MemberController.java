edbd73dbab1b669e71bb86fc4417e9416444a628
SSung <50323157+Sung2019@users.noreply.github.com>
2022-02-26 23:40:17 +0900

    Chapter 5-2 (1)

--- src/main/java/Practice1/practicespring/controller/MemberController.java
+++ src/main/java/Practice1/practicespring/controller/MemberController.java
@@ -1,8 +1,10 @@
 package Practice1.practicespring.controller;
 
+import Practice1.practicespring.domain.Member;
 import Practice1.practicespring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +13,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @GetMapping("/members/new")
+    public String create(MemberForm form){
+        // 멤버 객체 생성
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
