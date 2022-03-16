90d4a51a54e1f38b09b2c460cf869d70a31cec97
SSung <50323157+Sung2019@users.noreply.github.com>
2022-03-16 09:47:03 +0900

    Chapter 6-3
    스프링 DB 접근 기술 - 스프링 통합 테스트

---
+++ src/test/java/Practice1/practicespring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,62 @@
+package Practice1.practicespring.service;
+
+import Practice1.practicespring.domain.Member;
+import Practice1.practicespring.repository.MemberRepository;
+import Practice1.practicespring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void memberDuplicateException(){
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+//        try{
+//            memberService.join(member2);
+//            fail();
+//        }catch (IllegalStateException e){
+//            Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+
+
+        // then
+        Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+}
