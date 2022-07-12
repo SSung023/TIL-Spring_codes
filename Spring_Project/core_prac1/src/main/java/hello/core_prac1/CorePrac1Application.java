152d43c4c7396b9b6339875920ea1b43919118c3
SSung <50323157+Sung2019@users.noreply.github.com>
2022-07-12 15:27:18 +0900

    스프링 핵심 원리 : 기본편 - 연습 프로젝트 생성

---
+++ Spring_Project/core_prac1/src/main/java/hello/core_prac1/CorePrac1Application.java
@@ -0,0 +1,13 @@
+package hello.core_prac1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorePrac1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorePrac1Application.class, args);
+	}
+
+}
---
+++ Spring_Project/core_prac1/src/test/java/hello/core_prac1/CorePrac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.core_prac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorePrac1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
