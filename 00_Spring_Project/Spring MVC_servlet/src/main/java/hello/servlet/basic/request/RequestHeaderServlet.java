f965dd8f91e6e054669e0cf381afbc7c6cc41a2b
SSung <50323157+Sung2019@users.noreply.github.com>
2022-07-26 02:43:49 +0900

    Spring MVC 1 - 서블릿: GET 쿼리 파라미터
    Spring MVC 1 - 서블릿: GET 쿼리 파라미터

---
+++ 00_Spring_Project/Spring MVC_servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,96 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        printStartLine(request);
+        printHeaders(request);
+        printHeaderUtils(request);
+
+    }
+
+
+    private void printHeaderUtils(HttpServletRequest request) {
+        System.out.println("--- Header 편의 조회 start ---");
+
+        System.out.println("[Host 편의 조회]");
+        System.out.println("request.getServerName() = " +
+                request.getServerName()); //Host 헤더
+        System.out.println("request.getServerPort() = " +
+                request.getServerPort()); //Host 헤더
+        System.out.println();
+
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " + locale));
+        System.out.println("request.getLocale() = " + request.getLocale()); // Accept 정보 중 제일 우선순위 값이 높을 것은 꺼낸다
+        System.out.println();
+
+        System.out.println("[cookie 편의 조회]");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue());
+            }
+        }
+        System.out.println();
+
+        System.out.println("[Content 편의 조회]");
+        // GET 방식은 Content를 보내지 않기 때문에 결과값으로 null이 나온다.
+        System.out.println("request.getContentType() = " + request.getContentType());
+        System.out.println("request.getContentLength() = " + request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " + request.getCharacterEncoding());
+        System.out.println("--- Header 편의 조회 end ---");
+        System.out.println();
+    }
+
+
+    // Header의 모든 정보 출력
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println("--- Headers- start ---");
+
+        // .getHeaderNames() : Http의 모든 header 정보들을 가지고 옴
+/*        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            System.out.println(headerName + ": " + request.getHeader(headerName));
+        }*/
+
+        // 최근 버전
+        // 헤더를 하나만 얻고 싶다면 request.getHeader("Host")와 같이 사용
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> System.out.println(headerName + ": " + request.getHeader(headerName)));
+
+
+
+        System.out.println("--- Headers - end ---");
+        System.out.println();
+    }
+
+    private void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + request.getMethod()); //GET
+        System.out.println("request.getProtocol() = " + request.getProtocol()); // HTTP/1.1
+        System.out.println("request.getScheme() = " + request.getScheme()); //http
+        // http://localhost:8080/request-header
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        // /request-header
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        //username=hi
+        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure()); //https 사용 유무
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+}
