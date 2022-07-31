7dd40e510fd63b71970fe19eba1bfd30933c0e55
SSung <50323157+Sung2019@users.noreply.github.com>
2022-08-01 00:47:28 +0900

    스프링 MVC 1편 - 스프링 MVC 구조 이해
    핸들러 매핑(HandlerMapping)과 핸들러 어댑터(Handler Adapter)

---
+++ 00_Spring_Project/Spring MVC_servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+// 옛날에 쓰던 spring MVC Controller
+// spring bean의 이름을 url에 맞춰서 지정
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
