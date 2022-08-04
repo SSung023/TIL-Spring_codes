11a3709104bf15e7803eafe2f0a6c6d9cb4d5b41
SSung <50323157+Sung2019@users.noreply.github.com>
2022-08-04 23:56:33 +0900

    스프링 MVC 1편 : 스프링 MVC - 기본 기능
    HTTP 응답 - HTTP API, 메세지 바디에 직접 입력

---
+++ 00_Spring_Project/Spring MVC_spring boot/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,26 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1(){
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data", "hello!");
+
+        // @Controller에서 String을 반환하면 view의 논리적인 이름으로 인식한다.
+        return "response/hello";
+    }
+}
