95bbd8000110b7d897a44f339a4918c62d2086fd
SSung <50323157+Sung2019@users.noreply.github.com>
2022-08-12 11:44:53 +0900

    스프링 부트와 JPA 활용 1
    상품 도메인 개발

--- 00_Spring_Project/Spring JPA_01/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 00_Spring_Project/Spring JPA_01/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //== 비지니스 로직 : Entity 자체가 해결 할 수 있는 것들은 Entity 안에 비지니스 로직을 넣는다 ==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
