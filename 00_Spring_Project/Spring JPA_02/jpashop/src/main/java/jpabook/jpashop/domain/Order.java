7691d418d52dc68fb009dc1b028f8c5bde1991ab
SSung <50323157+Sung2019@users.noreply.github.com>
2022-08-18 10:06:55 +0900

    스프링 부트와 JPA 활용 2

---
+++ 00_Spring_Project/Spring JPA_02/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,106 @@
+package jpabook.jpashop.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    /**
+     * Order-Member 다대일 관계
+     * 연관관계 주인: 어떤 값이 변경되었을 때 FK를 바꿀거야!
+     * member_id(FK)를 연관관계 주인에 매핑 -> Order의 member를 연관관계의 주인으로 설정
+     */
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id") // FK의 이름이 member_id
+    private Member member;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; // hibernate가 지원
+
+    @Enumerated(EnumType.STRING) // 반드시 STRING으로 할 것
+    private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+
+
+    //== 연관관계 편의 메서드==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+
+    //== 생성 메서드: 생성 시에 모든 것을 설정 ==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        // 초기 상태를 주문 상태로 설정
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //== 비지니스 로직 ==//
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if (delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //== 조회 로직 ==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
+}
---
+++ 00_Spring_Project/Spring JPA_02/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // readOnly=true 로 설정하면 성능을 좀 더 올릴 수 있다.
+@RequiredArgsConstructor // final 필드만 가지고 생성자를 생성
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+    /**
+     * 회원 가입 + 중복 회원 검증
+     * 쓰기 기능에는 @Transactional을 추가하여 쓰기가 가능하도록(자세한 것이 우선순위가 높다)
+     */
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // DB에서 unique 제약 조건을 거는 것을 권장
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){ // EXCEPTION
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회 - readOnly
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    // 회원 단건 조회 - readOnly
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
