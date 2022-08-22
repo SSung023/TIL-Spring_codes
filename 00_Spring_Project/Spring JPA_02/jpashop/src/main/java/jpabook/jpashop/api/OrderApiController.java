384c959eab7accaad7fad4f6ce64c2d9fa92298b
SSung <50323157+Sung2019@users.noreply.github.com>
2022-08-22 16:01:50 +0900

    스프링 부트와 JPA 활용 2
    API 개발 고급 - 컬렉션 조회 최적화
    V1: 엔티티 직접 노출

---
+++ 00_Spring_Project/Spring JPA_02/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+
+    /**
+     * Entity를 직접 노출하는 방법
+     * 단점: Entity가 변하면 API spec도 변함 + 양방향 연관관계 문제(@JsonIgnore 추가 필요)
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
