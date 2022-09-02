4c3835e69dee110d954421141c72c49ac83d4e8e
SSung <50323157+Sung2019@users.noreply.github.com>
2022-09-02 09:38:25 +0900

    Update OrderServiceTest.java

--- 00_Spring_Project/Spring JPA_02_practice/jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ 00_Spring_Project/Spring JPA_02_practice/jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -1,6 +1,12 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.valueObject.Address;
+import jpabook.jpashop.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,18 +19,41 @@ class OrderServiceTest {
 
     @PersistenceContext
     EntityManager em;
-    
-    
+
+    @Autowired OrderService orderService;
+    @Autowired OrderRepository orderRepository;
+
+
     @Test
     public void 상품주문() throws Exception{
         // given
-        
+        Member member = createMember();
+        Book book = createBook("JPA1", 10000, 100);
+        int orderCount = 2;
+
         // when
+
+
         
         //then
         
     }
-    
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("userA");
+        member.setAddress(new Address("Seoul", "street1", "15673"));
+        em.persist(member);
+        return member;
+    }
+    private Book createBook(String name, int price, int quantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(quantity);
+        return book;
+    }
+
     @Test
     public void 주문취소() throws Exception{
         // given
