060962150737b696ca567e3039d78b61aed42fbd
SSung <50323157+Sung2019@users.noreply.github.com>
2022-09-19 15:45:03 +0900

    Feat : Add new Spring Project
    ORM JPA programming 책 프로젝트 생성

---
+++ 10_ORM JPA programming/jpaBookPrac/src/main/java/jpa/jpaBookPrac/JpaBookPracApplication.java
@@ -0,0 +1,13 @@
+package jpa.jpaBookPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBookPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBookPracApplication.class, args);
+	}
+
+}
